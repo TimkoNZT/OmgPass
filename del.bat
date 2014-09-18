4392dd0c40dcc38595699c212e0c73020794791e
Mrgnstrn <demonmind@mail.ru>
2014-09-18 23:41:56 +0600

    Изменен gitignore

--- del.bat
+++ del.bat
@@ -1,6 +1,10 @@
 del *.dcu
+del *.exe
 del *.local
 del *.identcache
-del win32\debug\*.dcu
-del win32\debug\*.exe
+;del win32\debug\*.dcu
+;del win32\debug\*.exe
+:del win32\release\*.dcu
+;del win32\release\*.exe
+rd win32 /q /s
 rd __history /q /s
